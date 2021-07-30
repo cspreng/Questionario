7e6abe66c0ae4929d8f0de9d3321fde2dcf5bced
cspreng <84556256+cspreng@users.noreply.github.com>
2021-07-30 02:06:30 -0300

    Delete questionario directory

--- questionario/src/main/java/com/questionario/questionario/repository/QuestionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.questionario.questionario.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.questionario.questionario.models.Cadastro;
-
-public interface QuestionarioRepository extends CrudRepository <Cadastro, String> {
-
-}
